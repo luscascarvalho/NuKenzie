e3f8d737f0fe130551bac9b9a9557d8e183dc1a4
lucas <lucascarvalho.mug@gmail.com>
2023-03-24 16:17:24 -0300

    Corrigindo apenas o caminho da Logo

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,7 +1,9 @@
+import Logo from "../assets/Logo.png";
+
 export const Header = () => {
   return (
     <header>
-      <img src="../src/assets/Nu Kenzie.svg" alt="Logo da NuKenzie" />
+      <img src={Logo} alt="Logo da NuKenzie" />
     </header>
   );
 };
